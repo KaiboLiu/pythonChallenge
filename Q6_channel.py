93f70e8d7e5d8cd6d53fa3819c3d4ac73efc8286
Kaibo Liu <liukaibo87@gmail.com>
2018-05-10 19:05:00 -0700

    update

--- Q6_channel.py
+++ Q6_channel.py
@@ -7,24 +7,25 @@ def test1():
 
 
 
-def q6(zfile,n,i,comment):
+def q6(zfile,n,i,comments):
     filename = n+'.txt'
 
     content = zfile.read(filename)
     newn = ''.join(re.findall('nothing is (\d+)',content))
+    # hint: collect the comments
     c1 = zfile.getinfo(filename).comment
     print i, content, '--', c1
 
     if newn:
-        q6(zfile,newn,i+1,comment+c1)
+        q6(zfile,newn,i+1,comments+c1)
     else: 
         print newn
-        print comment+zfile.getinfo(filename).comment
+        print comments+zfile.getinfo(filename).comment
 
     
 if __name__ == "__main__":
     url = 'http://www.pythonchallenge.com/pc/def/channel.html'
-    #test1()
+    test1()
     zfile = zipfile.ZipFile("Q6_channel.zip")
     q6(zfile, '90052', 0, '')
     #test2()
